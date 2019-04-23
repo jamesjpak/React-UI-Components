8755bf2d794188adf888cfae12cc6679bc61681a
EoPlex <you@example.com>
2019-04-23 14:12:21 -0700

    calculator button default styles created

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,56 @@
 import React from 'react';
 import './Display.css';
+
+ import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
+
+ActionButton.defaultProps = {
+    buttonStyle: 'default-red',
+};
+
+NumberButton.defaultProps = {
+    buttonStyle: 'punch-pad'
+}
+
+const CalculatorDisplay = () => {
+    return <div>
+
+        <div className = "layout">
+            <ActionButton buttonStyle = 'top-button' />
+        </div>
+
+        <div className = "layout">
+            <ActionButton buttonStyle = 'clear-button' />
+            <ActionButton buttonStyle = 'divide-button' />
+        </div>
+
+        <div className = "layout">
+            <NumberButton /> 
+            <NumberButton />
+            <NumberButton />
+            <ActionButton/>
+        </div>
+
+        <div className = "layout">
+            <NumberButton />
+            <NumberButton />
+            <NumberButton />
+            <ActionButton/>
+        </div>
+
+        <div className = "layout">
+            <NumberButton />
+            <NumberButton />
+            <NumberButton />
+            <ActionButton />
+        </div>
+
+        <div className = "layout">
+            <NumberButton />
+            <ActionButton />
+        </div>
+
+    </div>
+}
+
+export default CalculatorDisplay
